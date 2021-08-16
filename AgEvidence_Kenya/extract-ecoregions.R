00363ff04dd47ddbc0b2a1431a83a80d49bbc0ae
Steve Wood <stephenawood@gmail.com>
2021-08-16 14:47:54 -0400

    Update mapping data to FAO Farming Systems

--- AgEvidence_Kenya/extract-ecoregions.R
+++ AgEvidence_Kenya/extract-ecoregions.R
@@ -6,27 +6,27 @@ library(readxl)
 setwd("/Users/stephen.wood/Box Sync/Work/The Nature Conservancy/Global Soils/AgEvidence/code-and-data/AgEvidence_Kenya")
 
 # Add data
-kenya_ecoregions <- st_read("spatial-data/Ecozone Map Layers/Kenya_Ecoregions.shp") %>% st_make_valid()
+kenya_ecoregions <- st_read("spatial-data/Ecozone Map Layers/Kenya_Ecoregions.shp", crs = 4326) %>% 
+  st_make_valid()
+kenya_farming_systems <- st_read("spatial-data/FAO_Farming-Systems_KE/fao_farming-systems_ke.shp", crs = 4326) %>% 
+  st_make_valid()
 cc_points <- read_excel("data/ContinuousCover_Kenya.xlsx", sheet = "ExpD_Location")
 till_points <- read_excel("data/Tillage_Kenya.xlsx", sheet = "ExpD_Location")
 
 # Convert points to shapefiles
-cc_points <- st_as_sf(cc_points, coords = c("longitude", "latitude"), crs = 4326) %>% 
-  st_transform(st_crs(kenya_ecoregions))
-
-till_points <- st_as_sf(till_points, coords = c("longitude", "latitude"), crs = 4326) %>% 
-  st_transform(st_crs(kenya_ecoregions))
+cc_points <- st_as_sf(cc_points, coords = c("longitude", "latitude"), crs = 4326) 
+till_points <- st_as_sf(till_points, coords = c("longitude", "latitude"), crs = 4326)
 
 # Join with polygons to extract ecoregion information
-cc <- st_join(cc_points, kenya_ecoregions)
-till <- st_join(till_points, kenya_ecoregions)
+cc <- st_join(cc_points, kenya_farming_systems)
+till <- st_join(till_points, kenya_farming_systems)
 
 # Remove unnecessary columns
 cc_tb <- as_tibble(cc) %>% select(
-  paper_id:trtmt_splitC_levels,ECO_CODE,ECO_NAME,WWF_MHTNUM,WWF_MHTNAM
+  paper_id:trtmt_splitC_levels,DESCRIPTIO
 )
 till_tb <- as_tibble(till) %>% select(
-  paper_id:trtmt_splitC_levels,ECO_CODE,ECO_NAME,WWF_MHTNUM,WWF_MHTNAM
+  paper_id:trtmt_splitC_levels,DESCRIPTIO
 )
 
 # Write files
