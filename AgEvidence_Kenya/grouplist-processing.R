beb385689d52eb206b2defb7f42a27562503a626
LesleyAtwood <33636908+LesleyAtwood@users.noreply.github.com>
2021-11-09 13:37:10 -0500

    groups matched

--- AgEvidence_Kenya/grouplist-processing.R
+++ AgEvidence_Kenya/grouplist-processing.R
@@ -33,9 +33,13 @@ write.csv(group_lista, paste0("filtered-data/grouplists_Kenya_",Sys.Date(),".csv
 
 write.csv(till_groups, paste0("filtered-data/tilllists_Kenya_",Sys.Date(),".csv"))
 
+cc <- read_excel("data/ContinuousCover_Kenya_081721.xlsx", sheet = "Results")
+nm <- read_excel("data/NutrientMgmt_Kenya_CURRENT_081721.xlsx", sheet = "Results")
+till <- read_excel("data/Tillage_Kenya_081721.xlsx", sheet="Results")
 
 # Generate list of group discrepancies
-unique(unlist(strsplit(as.character(cc$trt2_name), ",")))
-unique(unlist(strsplit(as.character(nm$trt2_name), ";")))
+unique(unlist(strsplit(as.character(cc$mgmt_intention), ";")))
+unique(unlist(strsplit(as.character(nm$group_level3), ";")))
+unique(unlist(strsplit(as.character(till$group_level3), ";")))
 
           
