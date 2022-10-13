2f7224be59b1f09161fe896f37e0e9a46c7cfa7c
LesleyAtwood <lesley.atwood@gmail.com>
2022-10-13 12:50:40 -0400

    workbooks updated

--- AgEvidence_Kenya/datasets_web_complete_processing.R
+++ AgEvidence_Kenya/datasets_web_complete_processing.R
@@ -28,18 +28,19 @@ nm_k_web <- nm_k %>%
 library(openxlsx)
 
 #Cover Crops
-cc_wb <- loadWorkbook("data/ContinuousCover_Kenya_website.xlsx")
+cc_wb <- loadWorkbook("data/ContinuousCover_Kenya_complete.xlsx")
 openxlsx::removeWorksheet(cc_wb, "Results")
 openxlsx::addWorksheet(cc_wb, "Results")
 openxlsx::writeData(cc_wb, "Results", cc_k_new)
-openxlsx::saveWorkbook(cc_wb, "data/ContinuousCover_AgEKenya_website2.xlsx", overwrite = T)
+openxlsx::saveWorkbook(cc_wb, "data/ContinuousCover_Kenya_website.xlsx", overwrite = T)
+#manually remove paper_id 18 & 29
 
 #Nutrient Management
-nm_wb <- loadWorkbook("data/NutrientMgmt_Kenya_website.xlsx")
+nm_wb <- loadWorkbook("data/NutrientMgmt_Kenya_complete.xlsx")
 openxlsx::removeWorksheet(nm_wb, "Results")
 openxlsx::addWorksheet(nm_wb, "Results")
 openxlsx::writeData(nm_wb, "Results", nm_k_web)
-openxlsx::saveWorkbook(nm_wb, "data/NutrientMgmt_Kenya_website2.xlsx", overwrite = T)
+openxlsx::saveWorkbook(nm_wb, "data/NutrientMgmt_Kenya_website.xlsx", overwrite = T)
 #manually check for missing paper id's that don't move over.
 #remove paper_id = 5009 & 5013
 
