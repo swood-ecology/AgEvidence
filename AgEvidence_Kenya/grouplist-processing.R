68389f046b47d903335002214941d02219af3ba0
LesleyAtwood <lesley.atwood@gmail.com>
2022-11-18 15:41:40 -0500

    cleaned processing script, now inlcudes csv files

--- AgEvidence_Kenya/grouplist-processing.R
+++ AgEvidence_Kenya/grouplist-processing.R
@@ -10,10 +10,18 @@ library(readxl)
 library(plyr)
 library(ggplot2)
 
-# Data files
-cc <- read_excel("data/ContinuousCover_Kenya_complete.xlsx", sheet = "Results")
-nm <- read_excel("data/NutrientMgmt_Kenya_complete.xlsx", sheet = "Results")
-till <- read_excel("data/Tillage_Kenya_complete.xlsx", sheet="Results")
+# Data files, Excel Format ######
+#cc <- read_excel("data/ContinuousCover_Kenya_complete.xlsx", sheet = "Results")
+#nm <- read_excel("data/NutrientMgmt_Kenya_complete.xlsx", sheet = "Results")
+#till <- read_excel("data/Tillage_Kenya_complete.xlsx", sheet="Results")
+
+
+#Import Csv format ####
+
+cc <- read.csv("data/ContinuousCover/ContinuousCover_Kenya_complete_Results.csv")
+nm <- read.csv("data/NutrientMgmt/NutrientMgmt_Kenya_complete_Results.csv")
+till <- read.csv("data/Tillage/Tillage_Kenya_complete_Results.csv")
+
 
 
 # Generate nested grouping lists
@@ -28,9 +36,6 @@ group_list1 <- full_join(cc_groups, nm_groups)
 group_lista <- full_join(group_list1, till_groups)
 
 
-# Export csv
-write.csv(group_lista, paste0("filtered-data/grouplists_Kenya_",Sys.Date(),".csv"))
-
 
 # Generate list of group discrepancies
 unique(unlist(strsplit(as.character(cc$mgmt_intention), ";")))
@@ -39,9 +44,9 @@ unique(unlist(strsplit(as.character(till$group_level3), ";")))
 
 #Generate list of unique crops
 # Data files - Cash crop tab
-cc_crops <- read_excel("data/ContinuousCover_Kenya_complete.xlsx", sheet = "CashCrop")
-nm_crops <- read_excel("data/NutrientMgmt_Kenya_complete.xlsx", sheet = "CashCrop")
-till_crops <- read_excel("data/Tillage_Kenya_complete.xlsx", sheet="CashCrop")
+cc_crops <- read.csv("data/ContinuousCover/ContinuousCover_Kenya_complete_Cashcrop.csv")
+nm_crops <- read.csv("data/NutrientMgmt/NutrientMgmt_Kenya_complete_Cashcrop.csv")
+till_crops <- read.csv("data/Tillage/Tillage_Kenya_complete_Cashcrop.csv")
 
 cc_cropslist <- unique(unlist(strsplit(as.character(cc_crops$control_species), ";")))
 nm_cropslist <- unique(unlist(strsplit(as.character(nm_crops$control_species), ";")))
@@ -50,13 +55,14 @@ till_cropslist <- unique(unlist(strsplit(as.character(till_crops$cash_species), 
 crops <- unique(c(as.character(cc_cropslist), as.character(nm_cropslist), as.character(till_cropslist)))
 cropslist <- as.data.frame(crops)
 
-# Export csv with today's date pasted into file name
-#write.csv(dataframe, paste0("folder/filename_"),Sys.Date(),".csv"))
-write.csv(cropslist, paste0("filtered-data/cropslist_Kenya_",Sys.Date(),".csv"))
+
 
 
 
 ###########################################
+
+#explore website viewing data 
+#which practices get the most views?
 
 # Data files
 sitedata <- read_excel("/Users/lesley.atwood/Desktop/Active projects/AgEvidence/AgEvidenceSiteData.xlsx", 
